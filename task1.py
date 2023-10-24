644708cc0c006a804dd24782ad304aa5db474d8f
Rubyf246 <144410608+Rubyf246@users.noreply.github.com>
2023-10-24 13:05:22 -0700

    doneee

--- task1.py
+++ task1.py
@@ -1,6 +1,14 @@
+letter= input("Pick a letter from A to C      ")
+
 def A():
-
+    print("Hello")
 def B():
-
-
+    print("How are you?")
 def C():
+    print ("Hi")
+if letter == "A":
+    A()
+elif letter == "B":
+    B()
+else: 
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,18 @@ displays instructions and how to play
 game()
 plays the games
 """
+def title():
+    print("WELCOME TO KIMMIE'S GUESSING NUMBER GAME!!! \n You have to guess a number from 0 to 50, then Kimmie will tell you if your number is too high or too low. \n You have 7 guesses! \n GOOD LUCK ")
+title()
+num=47
+def game():
+    for i in range(1, 8):
+        numguess= int(input(f"put in your {i} guess"))
+        if  numguess==num:
+            print("YOU GOT IT!")
+            break 
+        elif numguess>num:
+            print("Guess is too high")
+        elif numguess<num:
+            print("Guess is too low")
+game()
